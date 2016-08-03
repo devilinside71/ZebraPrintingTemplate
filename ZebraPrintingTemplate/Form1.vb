5406a8a1d2ee296eb5491c4fc918b8f8cce5b365
Laszlo Tamas <laszlo.devil@gmail.com>
2016-08-03 08:49:37 +0200

    Start

---
+++ ZebraPrintingTemplate/Form1.vb
@@ -0,0 +1,32 @@
+﻿
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Call ZebraPrint.LoadPrinters()
+        ComboBoxPrinter.Items.Clear()
+        For Each pr In ZebraPrint.PrinterNames
+            ComboBoxPrinter.Items.Add(pr)
+        Next
+        ComboBoxPrinter.SelectedIndex = 0
+        Call ZebraPrint.LoadLabels()
+    End Sub
+    Sub ZPLPrint()
+        Dim strPrinter As String
+        Dim strPrintText As String
+        Dim res As String
+
+        Dim strTestText = "Almásrétes"
+
+        strPrinter = ZebraPrint.PrinterWinNames(ComboBoxPrinter.SelectedIndex)
+
+        strPrintText = ZebraPrint.LabelCodes(0)
+        strPrintText = strPrintText.Replace("UTF8STR", strTestText)
+        strPrintText = strPrintText.Replace("UTF8CODE", ZebraPrint.GetZPLutf8Code(strTestText))
+
+        res = ZebraPrint.SendStringToPrinter(strPrinter, strPrintText)
+
+    End Sub
+
+    Private Sub ButtonPrint_Click(sender As Object, e As EventArgs) Handles ButtonPrint.Click
+        Call ZPLPrint()
+    End Sub
+End Class
---
+++ ZebraPrintingTemplate/ZebraPrint.vb
@@ -0,0 +1,225 @@
+﻿Imports System.IO
+Imports System.Drawing.Printing
+Imports System.Runtime.InteropServices
+Imports System.Text
+Imports System.Web
+Imports System.Xml
+'http://labelary.com/viewer.html
+Public Class ZebraPrint
+
+    ' Structure and API declarions:
+    <StructLayout(LayoutKind.Sequential, CharSet:=CharSet.Unicode)>
+    Structure DOCINFOW
+        <MarshalAs(UnmanagedType.LPWStr)> Public pDocName As String
+        <MarshalAs(UnmanagedType.LPWStr)> Public pOutputFile As String
+        <MarshalAs(UnmanagedType.LPWStr)> Public pDataType As String
+    End Structure
+
+    <DllImport("winspool.Drv", EntryPoint:="OpenPrinterW",
+       SetLastError:=True, CharSet:=CharSet.Unicode,
+       ExactSpelling:=True, CallingConvention:=CallingConvention.StdCall)>
+    Public Shared Function OpenPrinter(ByVal src As String, ByRef hPrinter As IntPtr, ByVal pd As Long) As Boolean
+    End Function
+    <DllImport("winspool.Drv", EntryPoint:="ClosePrinter",
+       SetLastError:=True, CharSet:=CharSet.Unicode,
+       ExactSpelling:=True, CallingConvention:=CallingConvention.StdCall)>
+    Public Shared Function ClosePrinter(ByVal hPrinter As IntPtr) As Boolean
+    End Function
+    <DllImport("winspool.Drv", EntryPoint:="StartDocPrinterW",
+       SetLastError:=True, CharSet:=CharSet.Unicode,
+       ExactSpelling:=True, CallingConvention:=CallingConvention.StdCall)>
+    Public Shared Function StartDocPrinter(ByVal hPrinter As IntPtr, ByVal level As Int32, ByRef pDI As DOCINFOW) As Boolean
+    End Function
+    <DllImport("winspool.Drv", EntryPoint:="EndDocPrinter",
+       SetLastError:=True, CharSet:=CharSet.Unicode,
+       ExactSpelling:=True, CallingConvention:=CallingConvention.StdCall)>
+    Public Shared Function EndDocPrinter(ByVal hPrinter As IntPtr) As Boolean
+    End Function
+    <DllImport("winspool.Drv", EntryPoint:="StartPagePrinter",
+       SetLastError:=True, CharSet:=CharSet.Unicode,
+       ExactSpelling:=True, CallingConvention:=CallingConvention.StdCall)>
+    Public Shared Function StartPagePrinter(ByVal hPrinter As IntPtr) As Boolean
+    End Function
+    <DllImport("winspool.Drv", EntryPoint:="EndPagePrinter",
+       SetLastError:=True, CharSet:=CharSet.Unicode,
+       ExactSpelling:=True, CallingConvention:=CallingConvention.StdCall)>
+    Public Shared Function EndPagePrinter(ByVal hPrinter As IntPtr) As Boolean
+    End Function
+    <DllImport("winspool.Drv", EntryPoint:="WritePrinter",
+       SetLastError:=True, CharSet:=CharSet.Unicode,
+       ExactSpelling:=True, CallingConvention:=CallingConvention.StdCall)>
+    Public Shared Function WritePrinter(ByVal hPrinter As IntPtr, ByVal pBytes As IntPtr, ByVal dwCount As Int32, ByRef dwWritten As Int32) As Boolean
+    End Function
+
+    ' SendBytesToPrinter()
+    ' When the function is given a printer name and an unmanaged array of  
+    ' bytes, the function sends those bytes to the print queue.
+    ' Returns True on success or False on failure.
+    Public Shared Function SendBytesToPrinter(ByVal szPrinterName As String, ByVal pBytes As IntPtr, ByVal dwCount As Int32) As Boolean
+        Dim hPrinter As IntPtr      ' The printer handle.
+        Dim dwError As Int32        ' Last error - in case there was trouble.
+        Dim di As DOCINFOW          ' Describes your document (name, port, data type).
+        Dim dwWritten As Int32      ' The number of bytes written by WritePrinter().
+        Dim bSuccess As Boolean     ' Your success code.
+
+        ' Set up the DOCINFO structure.
+        With di
+            .pDocName = "TL RAW Data Printer"
+            .pDataType = "RAW"
+        End With
+        ' Assume failure unless you specifically succeed.
+        bSuccess = False
+        If OpenPrinter(szPrinterName, hPrinter, 0) Then
+            If StartDocPrinter(hPrinter, 1, di) Then
+                If StartPagePrinter(hPrinter) Then
+                    ' Write your printer-specific bytes to the printer.
+                    bSuccess = WritePrinter(hPrinter, pBytes, dwCount, dwWritten)
+                    EndPagePrinter(hPrinter)
+                End If
+                EndDocPrinter(hPrinter)
+            End If
+            ClosePrinter(hPrinter)
+        End If
+        ' If you did not succeed, GetLastError may give more information
+        ' about why not.
+        If bSuccess = False Then
+            dwError = Marshal.GetLastWin32Error()
+        End If
+        Return bSuccess
+    End Function ' SendBytesToPrinter()
+
+    ' SendFileToPrinter()
+    ' When the function is given a file name and a printer name, 
+    ' the function reads the contents of the file and sends the
+    ' contents to the printer.
+    ' Presumes that the file contains printer-ready data.
+    ' Shows how to use the SendBytesToPrinter function.
+    ' Returns True on success or False on failure.
+    Public Shared Function SendFileToPrinter(ByVal szPrinterName As String, ByVal szFileName As String) As Boolean
+        ' Open the file.
+        Dim fs As New FileStream(szFileName, FileMode.Open)
+        ' Create a BinaryReader on the file.
+        Dim br As New BinaryReader(fs)
+        ' Dim an array of bytes large enough to hold the file's contents.
+        Dim bytes(fs.Length) As Byte
+        Dim bSuccess As Boolean
+        ' Your unmanaged pointer.
+        Dim pUnmanagedBytes As IntPtr
+
+        ' Read the contents of the file into the array.
+        bytes = br.ReadBytes(fs.Length)
+        ' Allocate some unmanaged memory for those bytes.
+        pUnmanagedBytes = Marshal.AllocCoTaskMem(fs.Length)
+        ' Copy the managed byte array into the unmanaged array.
+        Marshal.Copy(bytes, 0, pUnmanagedBytes, fs.Length)
+        ' Send the unmanaged bytes to the printer.
+        bSuccess = SendBytesToPrinter(szPrinterName, pUnmanagedBytes, fs.Length)
+        ' Free the unmanaged memory that you allocated earlier.
+        Marshal.FreeCoTaskMem(pUnmanagedBytes)
+        Return bSuccess
+    End Function ' SendFileToPrinter()
+
+    ' When the function is given a string and a printer name,
+    ' the function sends the string to the printer as raw bytes.
+    Public Shared Function SendStringToPrinter(ByVal szPrinterName As String, ByVal szString As String)
+        Dim pBytes As IntPtr
+        Dim dwCount As Int32
+        ' How many characters are in the string?
+        dwCount = szString.Length()
+        ' Assume that the printer is expecting ANSI text, and then convert
+        ' the string to ANSI text.
+        pBytes = Marshal.StringToCoTaskMemAnsi(szString)
+        ' Send the converted ANSI string to the printer.
+        SendBytesToPrinter(szPrinterName, pBytes, dwCount)
+        Marshal.FreeCoTaskMem(pBytes)
+        Console.WriteLine("Print to " & szPrinterName)
+        Console.WriteLine(szString)
+        Return szString
+    End Function
+
+    'Change system keyboard to English
+    Public Shared Sub ChangeToEnKey()
+        Dim TypeOfLanguage = New System.Globalization.CultureInfo("en") ' or "fa-IR" for Farsi(Iran) 
+        InputLanguage.CurrentInputLanguage = InputLanguage.FromCulture(TypeOfLanguage)
+    End Sub
+
+    'Change system keyboard to Hungarian
+    Public Shared Sub ChangeToHuKey()
+        Dim TypeOfLanguage = New System.Globalization.CultureInfo("hu") ' or "fa-IR" for Farsi(Iran) 
+        InputLanguage.CurrentInputLanguage = InputLanguage.FromCulture(TypeOfLanguage)
+    End Sub
+
+    'Convert non ASCII characters to ZPL _XX_XX format
+    Public Shared Function GetZPLutf8Code(utf8text As String) As String
+        Dim result As String
+        result = vbNullString
+        For Each c In utf8text
+            If AscW(c) > 127 Then
+                result = result & UCase(HttpUtility.UrlEncode(c)).Replace("%", "_")
+            Else
+                result = result & c
+            End If
+        Next
+        Return result
+    End Function
+    Public Shared PrinterNames As List(Of String) = New List(Of String)
+    Public Shared PrinterWinNames As List(Of String) = New List(Of String)
+    Public Shared Sub LoadPrinters(Optional printers_filename As String = "Printers.xml")
+        Dim m_xmlr As XmlTextReader
+        'Create the XML Reader
+        m_xmlr = New XmlTextReader(printers_filename)
+        'Disable whitespace so that you don't have to read over whitespaces
+        m_xmlr.WhitespaceHandling = WhitespaceHandling.None
+        'read the xml declaration and advance to family tag
+        m_xmlr.Read()
+        'read the family tag
+        m_xmlr.Read()
+        'Load the Loop
+        While Not m_xmlr.EOF
+            'Go to the name tag
+            m_xmlr.Read()
+            'if not start element exit while loop
+            If Not m_xmlr.IsStartElement() Then
+                Exit While
+            End If
+            'Get the Gender Attribute Value
+            PrinterNames.Add(m_xmlr.GetAttribute("name"))
+            'Read elements firstname and lastname
+            m_xmlr.Read()
+            'Get the firstName Element Value
+            PrinterWinNames.Add(m_xmlr.ReadElementString("winname"))
+        End While
+        'close the reader
+        m_xmlr.Close()
+    End Sub
+    Public Shared LabelNames As List(Of String) = New List(Of String)
+    Public Shared LabelCodes As List(Of String) = New List(Of String)
+    Public Shared Sub LoadLabels(Optional labels_filename As String = "Labels.xml")
+        Dim m_xmlr As XmlTextReader
+        'Create the XML Reader
+        m_xmlr = New XmlTextReader(labels_filename)
+        'Disable whitespace so that you don't have to read over whitespaces
+        m_xmlr.WhitespaceHandling = WhitespaceHandling.None
+        'read the xml declaration and advance to family tag
+        m_xmlr.Read()
+        'read the family tag
+        m_xmlr.Read()
+        'Load the Loop
+        While Not m_xmlr.EOF
+            'Go to the name tag
+            m_xmlr.Read()
+            'if not start element exit while loop
+            If Not m_xmlr.IsStartElement() Then
+                Exit While
+            End If
+            'Get the Gender Attribute Value
+            LabelNames.Add(m_xmlr.GetAttribute("name"))
+            'Read elements firstname and lastname
+            m_xmlr.Read()
+            'Get the firstName Element Value
+            LabelCodes.Add(m_xmlr.ReadElementString("code"))
+        End While
+        'close the reader
+        m_xmlr.Close()
+    End Sub
+End Class
